31a998cbbd277085680e33ae42d949c8c5c2dada
Duane B <duanebednorz@aggienetwork.com>
2018-09-06 17:52:41 -0500

    style change load

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+   id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+  	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  	dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  	PRIMARY KEY(id)
+);
+
+
+SELECT * FROM burgers;
+
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'bluePEN456'
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Killer Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mountain-Stacked Avocado Jalepeno Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Dreamin Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('WTF Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burn Your Bottom Spicy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mad Turkey Burger', false);
